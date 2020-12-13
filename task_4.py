716b16f83437f01c08f5928d779d6b16b936ada8
abnatop <abnatop@gmail.com>
2020-12-13 14:56:49 +0300

    Задача 4. Basic defs for Car class.

--- task_4.py
+++ task_4.py
@@ -9,3 +9,41 @@ TownCar и WorkCar переопределите метод show_speed. При з
 Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к
 атрибутам, выведите результат. Выполните вызов методов и также покажите результат.
 """
+
+class Car:
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+        if speed == 0:
+            self.__drive = False
+        else:
+            self.__drive = True
+
+    def is_move(self):
+        return self.__drive
+
+    def go(self):
+        self.__drive = True
+        print('Car go.')
+
+    def stop(self):
+        self.__drive = False
+        print('Car stop.')
+
+    def turn(self, direction='home'):
+        if self.is_move():
+            print(f'Car turn to {direction}.')
+        else:
+            print(f'Car is not move.')
+
+
+cc = Car(speed=0, color='black', name='VOLVO')
+print(f'Car name is {cc.name}, color is {cc.color}')
+
+cc.turn()
+cc.go()
+cc.turn('left')
+cc.stop()
